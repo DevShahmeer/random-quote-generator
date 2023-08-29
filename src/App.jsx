93bff24307ee0513e5089f5f419e1d03d125cfc6
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-29 20:38:43 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import QuoteGenerator from './components/QuoteGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Random Quote Generator</h1>
+      <QuoteGenerator />
+    </div>
+  );
+}
+
+export default App;
